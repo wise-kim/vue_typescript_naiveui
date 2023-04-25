a64d8b3a2e173b9c48c6ea59fb15255244f27b94
wisekim <wisekim@ihopper.co.kr>
2023-04-25 18:43:11 +0900

    Apply: @naive-ui

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,77 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './style/reset.css'
+
+// naive-ui
+import {
+    create,
+    NMenu,
+    NTabs,
+    NTabPane,
+    NTab,
+    NLayout,
+    NLayoutHeader,
+    NLayoutSider,
+    NLayoutContent,
+    NGrid,
+    NGridItem,
+    NH2,
+    NH3,
+    NForm,
+    NFormItem,
+    NFormItemRow,
+    NInputGroup,
+    NSelect,
+    NRadioGroup,
+    NRadioButton,
+    NTable,
+    NUl,
+    NLi,
+    NButton,
+    NInput,
+    NSpace,
+    NDatePicker,
+    NInputGroupLabel,
+    NCheckboxGroup,
+    NCheckbox
+} from 'naive-ui'
+const naive = create({
+    components: [
+        NMenu,
+        NTabs,
+        NTab,
+        NTabPane,
+        NLayout,
+        NLayoutHeader,
+        NLayoutSider,
+        NLayoutContent,
+        NGrid,
+        NGridItem,
+        NH2,
+        NH3,
+        NForm,
+        NFormItem,
+        NFormItemRow,
+        NInputGroup,
+        NInputGroupLabel,
+        NSelect,
+        NRadioGroup,
+        NRadioButton,
+        NCheckboxGroup,
+        NCheckbox,
+        NTable,
+        NUl,
+        NLi,
+        NButton,
+        NInput,
+        NSpace,
+        NDatePicker
+    ]
+})
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router).use(naive)
 
 app.mount('#app')
